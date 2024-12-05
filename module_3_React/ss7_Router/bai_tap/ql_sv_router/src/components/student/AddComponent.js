57dc635b847caa3b9768aed86481f7ea428b3959
lean1835 <lean1835.vac@gmail.com>
2024-12-05 23:12:54 +0700

    form

--- module_3_React/ss7_Router/bai_tap/ql_sv_router/src/components/student/AddComponent.js
+++ module_3_React/ss7_Router/bai_tap/ql_sv_router/src/components/student/AddComponent.js
@@ -1,39 +1,56 @@
 import React,{useEffect,useRef,useState} from "react"
 import { addNewStudent } from "../../service/studentService";
 import {useNavigate} from "react-router-dom";
-function AddComponent({handleReload}){
-    const idRef=useRef();
-    const nameRef=useRef();
-    const phoneRef=useRef();
-    const emailRef=useRef();
+import {Formik,Form,Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup';
+function AddComponent(){
     const navigate=useNavigate();
-    const handleSave=(student)=>{
-        
+    const handleSubmit=(value)=>{
         console.log("hiiii");
-        addNewStudent({
-            id: idRef.current.value,
-            name: nameRef.current.value,
-            phone: phoneRef.current.value,
-            email: emailRef.current.value
-        })
-        alert("Thêm thành công");
-        // handleReload();
+        addNewStudent(value);
+        alert("Thêm thành côngn")
         navigate('/list');
     }
+    //check dinh dang
+    const handleValidate=Yup.object({
+        id:Yup.number().required("loi  ne").min(1,"Id phải là số nguyên dương"),
+        name:Yup.string().required("Loi chua nhap"),
+        phone:Yup.number().required("Loi chua nhap"),
+        email:Yup.string().required("Loi chua nhap").matches(/[A-Z][a-z]/,"hehe")
+    })
+    const [student,setStudent]=useState({id:'',name:'',phone:'',email:''});
     return (
         <>
-            <h2>Add Students</h2>
-            <form>
-                ID:
-                <input name={'id'} ref={idRef}/>
-                Name:
-                <input name={'name'} ref={nameRef}/>
-                Phone:
-                <input name={'phone'} ref={phoneRef}/>
-                Email:
-                <input name={'email'} ref={emailRef}/>
-                <button onClick={handleSave} type="button">Save</button>
-            </form>
+            <Formik initialValues={student} onSubmit={handleSubmit} validationSchema={handleValidate} >
+                <Form>
+                    <div>
+                        <label>Nhập id:</label>
+                        <Field type="number" name='id'/>
+                        <ErrorMessage name="id" component='div' style={{color: 'red'}}/>
+                    </div>
+                    <div>
+                        <label>Nhập name:</label>
+                        <Field type="text" name='name'/>
+                        <ErrorMessage name="name" component='div' style={{color: 'red'}}/>
+                    </div>
+                    <div>
+                        <label>Nhập phone:</label>
+                        <Field type="number" name='phone'/>
+                        <ErrorMessage name="phone" component='div' style={{color: 'red'}}/>
+                    </div>
+                    
+                    <div>
+                        <label>Nhập email:</label>
+                        <Field type="text" name='email'/>
+                        <ErrorMessage name="email" component='div' style={{color: 'red'}}/>
+                    </div>
+                    
+                    <div>
+                        <button type={'submit'}>Save</button>
+                    </div>
+                    
+                </Form>
+            </Formik>
         </>
 
     );
